87ae01afb644bbd7c4ec67d1271238b56c905c84
Ashraful Haque Chowdhury <ashraful.rahi@gmail.com>
2018-05-23 20:53:58 +0600

    Initial commit

---
+++ app/src/main/java/com/ashthebest/myauth/MainActivity.java
@@ -0,0 +1,119 @@
+package com.ashthebest.myauth;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView textViewSignUp;
+    private Button buttonLogin;
+    private EditText editTextEmail, editTextPassword;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        textViewSignUp = (TextView) findViewById(R.id.textViewSignup);
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+        progressBar = (ProgressBar) findViewById(R.id.progressbar);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+
+        textViewSignUp.setOnClickListener(this);
+        buttonLogin.setOnClickListener(this);
+
+    }
+
+    private void userLogin() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(email.isEmpty()) {
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Please Enter a valid Email");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()) {
+            editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if(password.length() < 6) {
+            editTextPassword.setError("Minimum length of password should be 6");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                progressBar.setVisibility(View.GONE);
+
+                if( task.isSuccessful() ) {
+                    finish();
+                    Intent intent = new Intent(MainActivity.this, ProActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if( mAuth.getCurrentUser() != null ) {
+            finish();
+            startActivity(new Intent(MainActivity.this, ProActivity.class));
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        /// This one will take you to the SignUp Activity
+        if( v.getId() == textViewSignUp.getId() ) {
+            finish();
+            startActivity(new Intent(this, SignUpActivity.class));
+        }
+        else if( v.getId() == buttonLogin.getId() ) {
+            userLogin();
+        }
+    }
+}
